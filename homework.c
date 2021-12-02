a7232ed586aa5397ec9f0d32c46f1460c6963d4f
Meteor <coopticks@outlook.com>
2021-12-02 20:43:10 +0800

    Beautify

--- homework.c
+++ homework.c
@@ -12,27 +12,24 @@ int main(void) {
 	t.tm_hour = 0;
 	t.tm_mday   = 16;
 	t.tm_mon    = 10-1;
-    t.tm_year   = 2002-1900;
-    
-    time_t birth = mktime(&t);
-    char buf[10];
-	int Flag = 0;
-	int len;
-	int counter = 0;
+    	t.tm_year   = 2002-1900;
+   	time_t birth = mktime(&t);
+    	char buf[10];
+	int Flag,counter,len,i;
 	for (; birth < 4102416000; birth+=86400){
 		Flag = 0;
 		strftime(buf, 10, "%Y%m%d", localtime(&birth));
 		len = strlen(buf);
-		for (int i=0;i < (len/2);++i) {
+		for (i=0;i < (len/2);++i) {
 			Flag = (buf[i] == buf[len-i-1]) ? 1 : 0;
 			if (!Flag) break;
 		}
 		if (Flag) {
-			printf("%s\n", buf);
+			printf("回文日期：%s\n", buf);
 			counter++;
 		}
 		
 	}
-	printf("%d", counter);
+	printf("总共回文日期数量：%d", counter);
 	return 0;
 }
