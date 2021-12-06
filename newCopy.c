29c595a6a5b747696c7979863d78cdb3a84564d9
Meteor <coopticks@outlook.com>
2021-12-06 14:46:45 +0800

    Add files via upload

---
+++ newCopy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void newCopy(char *str1, char *str2) {
+	char c;
+	while ((c = *str2++)) {
+		if (c < 97 || c  > 122) *str1++ = c;
+	}
+}
+int main(void)
+{
+	char a[] = "ABc";
+	char b[2];
+	newCopy(b,a);
+	printf("%s",b);
+	return 0;
+}
