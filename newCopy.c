717e03575bdd1ae5584f1579c071208b4a224246
Meteor <coopticks@outlook.com>
2021-12-06 14:49:27 +0800

    Update newCopy.c

--- newCopy.c
+++ newCopy.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 void newCopy(char *str1, char *str2) {
+	//It's not safe when we don't know how many lowcase characters in str2
+	//However it's just homework :)
 	char c;
 	while ((c = *str2++)) {
 		if (c < 97 || c  > 122) *str1++ = c;
