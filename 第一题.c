ebf232b4d22430c391e4c1a983f3d48e9c5ec4b6
Meteor <coopticks@outlook.com>
2021-12-09 13:43:27 +0800

    Add files via upload

---
+++ 第一题.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+unsigned long ConvertToInt(char *str) {
+	unsigned long num;
+	char c;
+	while (c = *str++) {
+		if (c >= '0' || c <= '9') {
+			num *= 10;
+			num += (c - '0');
+		}
+	}
+	return num;
+}
+
+int main(void)
+{
+	char str[100];
+	printf("请输入一串数字\n");
+	scanf("%s", str);
+	printf("转换为Int为： %lu\n", ConvertToInt(str));
+	return 0;
+}
